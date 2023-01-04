79bc3610ae31b3404818089a90aa05117c76d5f0
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-01-04 12:15:42 +0530

    Styled the Articles

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { categories } from "../constants";
 import fetchNews from "../lib/fetchNews";
+import NewsList from "./NewsList";
+import response from '../response.json'
 
 const HomePage = async () => {
-    const news: NewsResponse = await fetchNews(categories.join(','))
-
-    console.log(news);
+    const news: NewsResponse = response
+    // await fetchNews(categories.join(','))
 
     return <div>
-        HomePage
+        <NewsList news={news} />
         </div>
 }
 
